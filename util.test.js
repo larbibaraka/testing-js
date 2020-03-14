3c640de650393995f8f512ef9de55b3bd89a0847
Baraka Larbi <laridev@gmail.com>
2020-03-14 15:48:13 +0100

    adding our first e2e test

--- util.test.js
+++ util.test.js
@@ -1,4 +1,5 @@
 const { generateText, checkAndGenerate } = require("./util");
+const puppeteer = require("puppeteer");
 /**
  * unit tests
  */
@@ -18,4 +19,17 @@ test("should output data-less text without validation", () => {
 test("should generate a valid text output", () => {
   const text = checkAndGenerate("laridev", 25);
   expect(text).toBe("laridev (25 years old)");
+});
+
+/**
+ * e2e tesing using puppeteer
+ */
+
+test("should click around", async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ["--window-size=1920,1080"]
+  });
+  const page = await browser.newPage();
 });
