90f668e7d4e4da90d99a412b535fa6fea72c2dc1
Baraka Larbi <laridev@gmail.com>
2020-03-14 15:04:30 +0100

    unit test works

--- util.test.js
+++ util.test.js
@@ -1,6 +1,6 @@
 const { generateText } = require("./util");
 
-test("should output name and age", _ => {
+test("should output name and age", () => {
   const text = generateText("laridev", 25);
   expect(text).toBe("laridev (25 years old)");
 });
