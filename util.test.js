34480eb77fc9c29a9d40ee2c72e958ba0fa2c367
Baraka Larbi <laridev@gmail.com>
2020-03-14 15:10:37 +0100

    adding second unit test

--- util.test.js
+++ util.test.js
@@ -4,3 +4,8 @@ test("should output name and age", () => {
   const text = generateText("laridev", 25);
   expect(text).toBe("laridev (25 years old)");
 });
+
+test("should output data-less text without validation", () => {
+  const text = generateText("", null);
+  expect(text).toBe(" (null years old)");
+});
